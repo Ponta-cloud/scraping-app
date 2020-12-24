7b2ecc91604a9d8d7eed5e04760ff602126d1e24
Ponta-cloud <71808646+Ponta-cloud@users.noreply.github.com>
2020-12-25 08:33:41 +0900

    rspec編集

--- spec/models/event_detail_spec.rb
+++ spec/models/event_detail_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
-
 RSpec.describe EventDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event_detail) {create :event_detail}
+  
+  it "is updated if event_title is changed" do
+    event_detail.event_title = "運動会" 
+    expect(event_detail.event_title).not_to eq '【ポケごみ】親子deゴミ拾い散歩'
+  end
 end
